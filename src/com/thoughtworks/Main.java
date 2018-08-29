e12b2281c707662ac021681ee10683cad029bfb2
feixie-liam <xiefei0887@163.com>
2018-08-29 21:46:03 +0800

    add readme

--- src/com/thoughtworks/Main.java
+++ src/com/thoughtworks/Main.java
@@ -3,7 +3,7 @@ package com.thoughtworks;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         int count = 4;
 
         TriangleExercises triangleExercises = new TriangleExercises();
